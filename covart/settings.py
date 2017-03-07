8a6d45f40ea1fbdb9f647d0f292356681e527027
Chih-Yung Liang <stu89424@gmail.com>
2017-03-07 21:12:12 +0800

    Media(upload function) for dev purpose.

--- covart/settings.py
+++ covart/settings.py
@@ -120,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 MEETING_DAY = 0 # 0 for Monday
--- covart/urls.py
+++ covart/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
