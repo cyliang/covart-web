ca7fd5da8b20ac75886a884a854971766ad3ae9c
Chih-Yung Liang <stu89424@gmail.com>
2017-09-09 06:00:44 +0800

    Model for asset.

--- asset/admin.py
+++ asset/admin.py
@@ -2,5 +2,33 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ('label', 'name', 'date', 'custodian', 'description')
+    list_filter = ('name', 'date', 'custodian')
+    search_fields = ('label', 'description', 'remark')
+
+    def get_changeform_initial_data(self, request):
+        from website.models import Member
+
+        try:
+            return {
+                'custodian': Member.objects.get(identity=Member.ADVISOR),
+            }
+        except:
+            return {}
+
+    def save_model(self, request, obj, form, change):
+        super(AssetAdmin, self).save_model(request, obj, form, change)
+
+        if not change:
+            obj.transferlog_set.create(status=models.TransferLog.BUY)
+
+
+@admin.register(models.TransferLog)
+class TransferLogAdmin(admin.ModelAdmin):
+    list_display = ('asset', 'status', 'time', 'new_user', 'location')
+    list_filter = ('time',)
+    search_fields = ('asset__label',)
--- asset/models.py
+++ asset/models.py
@@ -2,5 +2,69 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class Asset(models.Model):
+    label       = models.CharField('產標編號', help_text='Ex: xxxxxxxxxx-yyyyy',
+                                   unique=True, max_length=255)
+    name        = models.CharField('物品類別', help_text='Ex: 行動電話機',
+                                   max_length=255)
+    date        = models.DateField('產標日期')
+    years       = models.SmallIntegerField('年限')
+    custodian   = models.CharField('保管人', help_text='Ex: 徐慰中',
+                                   max_length=50)
+    description = models.CharField('描述', help_text='Ex: iPhone 6 plus',
+                                   max_length=255)
+    remark      = models.TextField('備註', help_text='Ex: 密碼是xxxx',
+                                   blank=True)
+
+    class Meta:
+        verbose_name = '財產'
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return '%s: %s (%s)' % (self.label, self.name, self.description)
+
+    @property
+    def latest_log(self):
+        return self.transferlog_set.latest()
+
+
+class TransferLog(models.Model):
+    BUY      = 'BUY'
+    TRANSFER = 'TRANSFER'
+    RETURN   = 'RETURN'
+    SCRAP    = 'SCRAP'
+    STATUS_CHOICES = (
+        (BUY, '購入'),
+        (TRANSFER, '轉移/取用'),
+        (RETURN, '歸還'),
+        (SCRAP, '報廢'),
+    )
+
+    status   = models.CharField(choices=STATUS_CHOICES, max_length=20)
+    time     = models.DateTimeField(auto_now_add=True)
+    asset    = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    new_user = models.ForeignKey('website.Member', null=True, blank=True, on_delete=models.SET_NULL)
+    location = models.CharField(blank=True, max_length=255, help_text='Ex: 座位')
+    remark   = models.CharField(blank=True, max_length=255, help_text='物品狀態(壞了?)、用途說明')
+
+    class Meta:
+        unique_together = ('asset', 'time')
+        ordering = ['-time']
+        get_latest_by = 'time'
+
+    def __unicode__(self):
+        return unicode(self.asset) + self.get_status_display()
+
+    def clean(self):
+        super(TransferLog, self).clean()
+
+        if self.asset.transferlog_set.filter(status=self.SCRAP).exclude(pk=self.pk).exists():
+            raise ValidationError('This asset is already scrapped.')
+        if self.status == self.BUY and self.pk is None:
+            raise ValidationError('The status "BUY" shall only be created automatically.')
+        if self.status == self.TRANSFER and self.new_user is None:
+            raise ValidationError('Missing the transfer target (the new user).')
+        if self.status in (self.RETURN, self.SCRAP) and self.new_user is not None:
+            raise ValidationError("There shouldn't be new user for a returned/scrapped asset.")
