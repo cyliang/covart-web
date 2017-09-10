c5530747233247aaa7f44ea2a024e9859128c4e1
Chih-Yung Liang <stu89424@gmail.com>
2017-09-11 02:12:08 +0800

    Allow members to transfer/return assets to/from themselves.

--- asset/urls.py
+++ asset/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.AssetTableView.as_view(), name='all'),
+    url(r'^transfer/(?P<asset>.+)$', views.LogTransferFormView.as_view(), name='transfer'),
     url(r'^(?P<slug>.+)$', views.AssetDetailView.as_view(), name='detail'),
 ]
 
--- asset/views.py
+++ asset/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.views.generic import DetailView
+from django.shortcuts import get_object_or_404, Http404
+from django.views.generic import DetailView, CreateView
 from django.db.models import Sum, Case, When, BooleanField
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django_tables2 import SingleTableView
 from django_filters.views import FilterMixin
 from . import models, tables, filters
@@ -41,3 +42,57 @@ class AssetDetailView(LoginRequiredMixin, DetailView):
     model = models.Asset
     slug_field = 'label'
     template_name = 'asset/detail.html'
+
+
+class AssetLoggingMixin(object):
+    asset_url_kwarg = 'asset'
+
+    def get_asset(self):
+        return get_object_or_404(
+            models.Asset,
+            label=self.kwargs[self.asset_url_kwarg],
+        )
+
+    def dispatch(self, request, *args, **kwargs):
+        self.asset = self.get_asset()
+        if self.asset.latest_log.status == models.TransferLog.SCRAP:
+            raise Http404('This is not a mutable asset.')
+
+        return super(AssetLoggingMixin, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(AssetLoggingMixin, self).get_context_data(**kwargs)
+        context['asset'] = self.asset
+        return context
+
+    def get_success_url(self):
+        return self.asset.get_absolute_url()
+
+
+class LogTransferFormView(LoginRequiredMixin, AssetLoggingMixin, CreateView):
+    model = models.TransferLog
+    fields = ('location', 'remark')
+    template_name = 'asset/member_transfer.html'
+
+    def get_form_kwargs(self):
+        kwargs = super(LogTransferFormView, self).get_form_kwargs()
+
+        if self.asset.latest_log.new_user != self.request.user.member:
+            kwargs['instance'] = self.model(
+                status=models.TransferLog.TRANSFER,
+                asset=self.asset,
+                new_user=self.request.user.member,
+            )
+        else:
+            kwargs['instance'] = self.model(
+                status=models.TransferLog.RETURN,
+                asset=self.asset,
+                new_user=None,
+            )
+
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super(LogTransferFormView, self).get_context_data(**kwargs)
+        context['action'] = ['take', 'return'][self.asset.latest_log.new_user == self.request.user.member]
+        return context
